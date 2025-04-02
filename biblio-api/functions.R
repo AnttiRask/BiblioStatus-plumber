41a8dde8ded88ab2dead35f6b6c9cd5125edac50
Antti Rask <anttilennartrask@gmail.com>
2025-04-03 02:09:10 +0300

    Debugging, part 11

--- biblio-api/functions.R
+++ biblio-api/functions.R
@@ -1,6 +1,7 @@
 # Function to fetch libraries from SQLite
 fetch_libraries <- function() {
   con <- dbConnect(SQLite(), here("libraries.sqlite"))
+  # con <- dbConnect(SQLite(), here("biblio-api/libraries.sqlite")) # Uncomment for the local version
   on.exit(dbDisconnect(con))
   dbReadTable(con, "libraries")
 }
@@ -8,6 +9,7 @@ fetch_libraries <- function() {
 # Function to fetch schedules from SQLite
 fetch_schedules <- function() {
   con <- dbConnect(SQLite(), here("libraries.sqlite"))
+  # con <- dbConnect(SQLite(), here("biblio-api/libraries.sqlite")) # Uncomment for the local version
   on.exit(dbDisconnect(con))
   dbReadTable(con, "schedules")
 }
